e736b911975ae1003644bc92a9d1d51f582beb3f
SJ <behpyall@gmail.com>
2026-07-17 11:16:33 +0900

    chore(backend): 애플리케이션 진입점 및 기본 테스트

---
+++ open_forms_backend/src/main/java/com/openforms/OpenFormsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.openforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFormsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFormsBackendApplication.class, args);
+	}
+
+}
---
+++ open_forms_backend/src/test/java/com/openforms/OpenFormsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFormsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
